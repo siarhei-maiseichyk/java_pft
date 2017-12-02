39e8d37c2bea9e02c7bd16d6f99799a4d6bb0878
siarhei-maiseichyk <jaguar_sergey@tut.by>
2017-12-02 16:12:46 +0300

    Добавлен класс GroupModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(FirefoxDriver wd) {
         super(wd);
@@ -12,4 +12,9 @@ public class NavigationHelper extends HelperBase{
     public void gotoGroupPage() {
         click(By.linkText("groups"));
     }
+
+    public void returnToGroupPage() {
+        click(By.linkText("groups"));
+    }
+
 }
